a378cd769514b4e85af81954db8a784e7aa0862f
mireia22 <mireiagarciaferrer22@gmail.com>
2023-09-05 13:18:49 +0200

    change img src

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
       <p>Created by Mire</p>
       <AncoreWrp>
         <a href="https://github.com/mireia22">
-          <img src="/github.png" alt="github" />
+          <img src="/assets/images/github.png" alt="github" />
         </a>
         <a href="https://www.linkedin.com/in/mireia-garcia-ferrer-40381b255/">
-          <img src="/linkedin.png" alt="linkedin" />
+          <img src="/assets/images/linkedin.png.png" alt="linkedin" />
         </a>
       </AncoreWrp>
     </FooterWrp>
